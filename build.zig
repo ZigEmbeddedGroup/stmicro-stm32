f8be648739b67e9945bb8388096ad3b8399f4920
Matt Knight <mattnite@protonmail.com>
2023-02-18 11:33:36 -0500

    add chips to build

--- build.zig
+++ build.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const microzig = @import("deps/microzig/src/main.zig");
 const boards = @import("src/boards.zig");
+const chips = @import("src/chips.zig");
 
 pub fn build(b: *std.build.Builder) void {
     const optimize = b.standardOptimizeOption(.{});
@@ -10,9 +11,23 @@ pub fn build(b: *std.build.Builder) void {
 
         const exe = microzig.addEmbeddedExecutable(
             b,
-            decl.name ++ ".minimal",
+            @field(boards, decl.name).name ++ ".minimal",
             "test/programs/minimal.zig",
             .{ .board = @field(boards, decl.name) },
+            .{ .optimize = optimize },
+        );
+        exe.install();
+    }
+
+    inline for (@typeInfo(chips).Struct.decls) |decl| {
+        if (!decl.is_pub)
+            continue;
+
+        const exe = microzig.addEmbeddedExecutable(
+            b,
+            @field(chips, decl.name).name ++ ".minimal",
+            "test/programs/minimal.zig",
+            .{ .chip = @field(chips, decl.name) },
             .{ .optimize = optimize },
         );
         exe.install();
