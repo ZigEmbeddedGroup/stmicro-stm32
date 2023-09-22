2b959039cdea90ac3950f9a7366b24b05e150593
Francisco <franciscomaggi1999@gmail.com>
2023-09-22 17:33:19 -0300

    better interface

--- src/hals/STM32F103/gpio.zig
+++ src/hals/STM32F103/gpio.zig
@@ -16,12 +16,7 @@ const Port = @TypeOf(GPIOA);
 
 const log = std.log.scoped(.gpio);
 
-pub const Direction = enum(u1) {
-    in,
-    out,
-};
-
-pub const Mode = union {
+pub const Mode = union(enum) {
     input: InputMode,
     output: OutputMode,
 };
@@ -56,25 +51,6 @@ pub const IrqLevel = enum(u2) {
 
 pub const IrqCallback = fn (gpio: u32, events: u32) callconv(.C) void;
 
-pub const Override = enum {
-    normal,
-    invert,
-    low,
-    high,
-};
-
-pub const SlewRate = enum {
-    slow,
-    fast,
-};
-
-pub const DriveStrength = enum {
-    @"2mA",
-    @"4mA",
-    @"8mA",
-    @"12mA",
-};
-
 pub const Enabled = enum {
     disabled,
     enabled,
@@ -91,18 +67,34 @@ pub const Pin = packed struct(u8) {
     port: u3,
     padding: u1,
 
-    // this could probably be a type
     // according to the manual CRL and CRH registers have to be accessed as 32-bit words
     // We could try accessing them as 4-bit words to acces only the needed bits per pin
     // But it will probably not work
-    pub const ConfigReg = u32;
+    pub const ConfigReg = struct {
+        reg: u32,
+
+        pub inline fn write_pin_config(self: ConfigReg, gpio: Pin, config: u4) void {
+            const offset = if (gpio.number <= 7)
+                gpio.number << 2
+            else
+                (gpio.number - 8) << 2;
+
+            const state = self.reg;
+            const clear_msk: u32 = ~(0b1111 << offset);
+            self.reg = (state & clear_msk) | config << offset;
+        }
+    };
+
+    fn mask(gpio: Pin) u16 {
+        return @as(u16, 1) << gpio.number;
+    }
 
     fn get_config_reg(gpio: Pin) *volatile ConfigReg {
         const port = gpio.get_port();
         return if (gpio.number <= 7)
-            &port.CRL.raw
+            @ptrCast(&port.CRL.raw)
         else
-            &port.CRH.raw;
+            @ptrCast(&port.CRH.raw);
     }
 
     pub fn get_port(gpio: Pin) Port {
@@ -118,54 +110,38 @@ pub const Pin = packed struct(u8) {
         }
     }
 
-    pub inline fn set_direction(gpio: Pin, direction: Direction, mode: ?Mode, speed: ?Speed) void {
-        switch (direction) {
-            .in => gpio.set_in_mode(mode.?.input),
-            .out => gpio.set_out_mode(mode.?.output, speed),
+    pub inline fn set_mode(gpio: Pin, mode: Mode, speed: ?Speed) void {
+        switch (mode) {
+            .input => |in| gpio.set_input_mode(in),
+            .output => |out| gpio.set_output_mode(out, speed.?),
         }
     }
 
-    pub inline fn set_in_mode(gpio: Pin, mode: ?InputMode) void {
+    pub inline fn set_input_mode(gpio: Pin, mode: InputMode) void {
         const config_reg = gpio.get_config_reg();
-        var ioconfig = 0;
-        if (mode) |m| {
-            ioconfig = @intFromEnum(m) << 2;
-        } else {
-            ioconfig = @intFromEnum(InputMode.floating) << 2;
-        }
-        const offset = if (gpio.number <= 7)
-            gpio.number << 2
-        else
-            (gpio.number - 8) << 2;
-
-        const state = config_reg;
-        const clear_msk: u32 = ~(0x15 << offset);
-        config_reg = (state & clear_msk) | ioconfig;
+        const config = @intFromEnum(mode) << 2;
+        config_reg.write_pin_config(gpio, config);
     }
 
-    pub inline fn set_out_mode(gpio: Pin, mode: ?OutputMode, speed: ?Speed) void {
+    pub inline fn set_output_mode(gpio: Pin, mode: OutputMode, speed: Speed) void {
         const config_reg = gpio.get_config_reg();
-        var ioconfig = 0;
-        if (mode) |m| {
-            ioconfig = @intFromEnum(m) << 2;
-        } else {
-            ioconfig = @intFromEnum(OutputMode.general_purpose_push_pull) << 2;
-        }
+        const config = @intFromEnum(speed) + @intFromEnum(mode) << 2;
+        config_reg.write_pin_config(gpio, config);
+    }
 
-        var speedconfig = 0x2;
-        if (speed) |s| {
-            speedconfig = @intFromEnum(s);
-        }
-
-        ioconfig += speedconfig;
-
-        const offset = if (gpio.number <= 7)
-            gpio.number << 2
+    pub inline fn read(gpio: Pin) u1 {
+        const port = gpio.get_port();
+        return if (port.IDR.raw & gpio.mask() != 0)
+            1
         else
-            (gpio.number - 8) << 2;
+            0;
+    }
 
-        const state = config_reg;
-        const clear_msk: u32 = ~(0x15 << offset);
-        config_reg = (state & clear_msk) | ioconfig;
+    pub inline fn put(gpio: Pin, value: u1) void {
+        const port = gpio.get_port();
+        switch (value) {
+            0 => port.BSSR = gpio.mask() << 16,
+            1 => port.BSSR = gpio.mask(),
+        }
     }
 };
